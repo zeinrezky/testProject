b0fe63c12f84132b0a6cc36bbc129364934180cb
Zein Chandra <zeinrezky@gmail.com>
2018-06-28 13:36:43 +0700

    Initial Commit

---
+++ asd/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  asd
+//
+//  Created by zein rezky chandra on 28/06/18.
+//  Copyright © 2018 Zein. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
