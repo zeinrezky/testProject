ca009cbe4089b561f3547f7c0433c8256e503a93
Zein Chandra <zeinrezky@gmail.com>
2018-07-03 16:24:59 +0700

    - haduh

--- asd/ViewController.swift
+++ asd/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        setup()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +21,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func setup(){
+        
+    }
+    
 }
 
