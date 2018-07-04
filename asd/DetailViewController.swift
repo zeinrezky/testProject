ce01db7bf6fde93168b9c5b6bc3c729bd27b48a2
zein rezky chandra <zeinrezkychandra@10-60-49-112.0200.bsd.id.iosda.org>
2018-07-04 15:03:18 +0700

    Tony ini file gw, pake ini ya

---
+++ asd/DetailViewController.swift
@@ -0,0 +1,26 @@
+//
+//  DetailViewController.swift
+//  asd
+//
+//  Created by zein rezky chandra on 04/07/18.
+//  Copyright © 2018 Zein. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    @IBOutlet weak var myImage: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
