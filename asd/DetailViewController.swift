dbf5b10c3d3c8aa0f7af8d7394480d4c34e27cc8
zein rezky chandra <zeinrezkychandra@10-60-49-112.0200.bsd.id.iosda.org>
2018-07-04 15:10:23 +0700

    my version

--- asd/DetailViewController.swift
+++ asd/DetailViewController.swift
@@ -10,17 +10,42 @@ import UIKit
 
 class DetailViewController: UIViewController {
     
-    @IBOutlet weak var myImage: UIImageView!
-
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        setupConfig()
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func setupConfig(){
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+}
 
+extension DetailViewController: UITableViewDataSource{
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+}
+
+extension DetailViewController: UITableViewDelegate{
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 44
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellStatic = tableView.dequeueReusableCell(withIdentifier: "asd")!
+        return cellStatic
+    }
 }
---
+++ asd/MyCell.swift
@@ -0,0 +1,24 @@
+//
+//  MyCell.swift
+//  asd
+//
+//  Created by zein rezky chandra on 04/07/18.
+//  Copyright © 2018 Zein. All rights reserved.
+//
+
+import UIKit
+
+class MyCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
